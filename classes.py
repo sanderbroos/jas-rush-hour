7dc513db18a77eb5b5532111ea0bcf98cbf205a5
akkostu <alcomoerman@gmail.com>
2021-01-08 14:42:39 +0100

    initial datastructure in code

---
+++ classes.py
@@ -0,0 +1,51 @@
+
+
+class Board():
+
+    def __init__ (self, size):
+        # load board and everything on it
+        
+        def load_board(self):
+            pass
+
+        def load_cars(self):
+            pass
+
+        def load_trucks(self):
+            pass
+
+
+    def draw_board(self):
+        # draw board and everthing on it
+        pass
+    
+    def move(self, direction):
+        # move trucks or cars
+        pass
+    
+    def won(self):
+        # game won?
+        pass
+    
+    def is_validMove(self):
+        pass
+ 
+
+class Car():
+
+    def __init__(self,orientation):
+        self.id = id #dont know of dit goed gaat..
+        self.orientation = orientation
+        self.color = "some random color"
+        self.length = 2
+
+
+
+class Truck():
+    # voelt een beetje dubbel dit.. als je al die class
+
+    def __init__(self,orientation):
+        self.id = id #dont know of dit goed gaat..
+        self.orientation = orientation
+        self.color = "some random color"
+        self.length = 3
