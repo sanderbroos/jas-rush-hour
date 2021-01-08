8b327df5beb4f75723ef94c822a042cf15e88457
akkostu <alcomoerman@gmail.com>
2021-01-08 19:57:31 +0100

    added create_board function and attribute board

--- code/classes/board.py
+++ code/classes/board.py
@@ -4,11 +4,13 @@ from .car import Car
 
 
 class Board:
+
     def __init__ (self, size, source_file):
         self.size = size
 
         # load board and everything on it
         self.cars = self.load_cars(source_file)
+        self.board = self.create_board()
         
     def load_cars(self, source_file):
         # load dictionary with key name car and value the car 
@@ -24,20 +26,26 @@ class Board:
                 cars[row['car']] = Car(row['car'], row['orientation'], int(row['col']), int(row['row']), int(row['length']), carX)
 
         return cars
-            
-    def draw_board(self):
-        # draw board and everthing on it
-        
+    
+
+    def create_board(self):
+
         # initialize empty 2D list representation of board
-        to_draw = [['_' for i in range(self.size)] for j in range(self.size)]
+        board = [['_' for i in range(self.size)] for j in range(self.size)]
 
         for car in self.cars.values():
             for i in range(car.length):
                 if car.orientation == 'H':
-                    to_draw[car.row - 1][car.col - 1 + i] = car.name
+                    board[car.row - 1][car.col - 1 + i] = car.name
                 if car.orientation == 'V':
-                    to_draw[car.row - 1 + i][car.col - 1] = car.name
+                    board[car.row - 1 + i][car.col - 1] = car.name
+
+        return board
         
+    def draw_board(self):
+        # draw board and everthing on it
+        to_draw = self.board
+
         for row in to_draw:
             for item in row:
                 print(f'{item} ', end='')
@@ -45,11 +53,16 @@ class Board:
     
     def move(self, direction):
         # move trucks or cars, check is validmove etc the save new place in car class, save moves made for output file
+        
         pass
-    
+
     def won(self):
         # game won, car in space just before gate means game is won
         pass
     
-    def is_valid_move(self):
+    def is_valid_move(self, car):
         pass
+
+        
+        
+        
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from code.classes import board
+from code.classes.board import Board
 
 if __name__ == "__main__":
-    board = board.Board(6, "data/Rushhour6x6_1.csv")
+    board = Board(6, "data/Rushhour6x6_1.csv")
     board.draw_board()
