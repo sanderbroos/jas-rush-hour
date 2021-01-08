2d2814cbb2b2311f61a3da4760cffadc5e52a632
Jamie Faber <jamiefaber@hotmail.com>
2021-01-08 15:25:10 +0100

    folder changes and updated classes

---
+++ code/classes/board.py
@@ -0,0 +1,27 @@
+from .car import Car
+
+class Board():
+
+    def __init__ (self, size):
+        self.size = size
+        # load board and everything on it
+        
+        def load_cars(self):
+            # load dictionary with key name car and value the car
+            pass
+
+
+    def draw_board(self):
+        # draw board and everthing on it
+        pass
+    
+    def move(self, direction):
+        # move trucks or cars, check is validmove etc the save new place in car class, save moves made for output file
+        pass
+    
+    def won(self):
+        # game won, car in space just before gate means game is won
+        pass
+    
+    def is_validMove(self):
+        pass
---
+++ code/classes/car.py
@@ -0,0 +1,9 @@
+class Car():
+
+    def __init__(self, name, orientation, col, row, length, carX):
+        self.name = name
+        self.orientation = orientation
+        self.col = col
+        self.row = row
+        self.length = length
+        self.carX = carX
