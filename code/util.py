c4b45dcbf19bf8ca3d7c8fc93e582b24e87e6d3c
akkostu <alcomoerman@gmail.com>
2021-01-11 14:17:58 +0100

    added possiblities util function

--- code/util.py
+++ code/util.py
@@ -17,7 +17,36 @@ def get_lane(car, board):
 
 
 
-def get_possiblities(car, board):
-    #alle mogelijke moves van een auto
+def get_possiblities(car, board, size):
+    """
+    gives all possible moves of a car in a given lane
+    """
 
-    pass
+    moves = []
+    row = car.row
+    col = car.col
+
+    if car.orientation == 'H':
+        for i in range(1, size - 1):
+            if (col + i + car.length < size and
+                    board[row][col  + (i + car.length)] is None):
+                moves.append(i)
+            elif (col - i > -1
+                    and board[row][col - i] is None):
+                moves[:0] = [-i]
+            else:
+                break
+    
+    if car.orientation == 'V':
+        for i in range(1, size - 1):
+            if (row + i + car.length < size and
+                    board[row + (i + car.length)][col] is None):
+                moves.append(i)
+            elif (row - i > -1
+                    and board[row- i][col] is None):
+                moves[:0] = [-i]
+            else:
+                break
+        
+    return moves
+
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from code.classes.game import Game
-from code.util import get_lane
+from code.util import get_lane, get_possiblities
 
 if __name__ == "__main__":
     game = Game(6, "data/Rushhour6x6_1.csv")
     game.draw_board()
-
-
-
