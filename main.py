eb116f8657b35f85be92843b6bea578df6f979d1
sanderbroos <sander.broos@student.uva.nl>
2021-01-12 16:18:26 +0100

    Divided main.py into sections

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from code.algorithms.randomise import *
 
 if __name__ == "__main__":
     game = Game(6, "data/Rushhour6x6_1.csv")
-    print('Instructions: Left and down are negative numbers, right and up are positive numbers')
     game.draw_board()
+
+    # --------------------------- Randomise --------------------------
 
     best_yet = 9999999
     N = int(input("How many times do you want to run the randomise algorithm?\n"))
@@ -26,11 +27,13 @@ if __name__ == "__main__":
             best_yet = len(game.moves)
             game.output()
             game.draw_board()
-            
+
     game.draw_board()
 
+    # --------------------------- Manual -----------------------------
 
     # while not game.won():
+    #     print('Instructions: Left and down are negative numbers, right and up are positive numbers')
     #     car = input("Please the name of the car that should be moved:\n")
     #     if car == "stop":
     #         break
@@ -38,5 +41,4 @@ if __name__ == "__main__":
     #     direction = input("Please enter your move from -4 to 4:\n")
     #     game.move(car.upper(), int(direction))
     #     game.draw_board()
-
-    
+    #     game.output()
