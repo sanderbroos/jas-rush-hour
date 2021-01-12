c155a0939600dde4e405759e0bf8f8eacc011f71
sanderbroos <sander.broos@student.uva.nl>
2021-01-12 17:22:20 +0100

    Small fix

--- main.py
+++ main.py
@@ -9,35 +9,36 @@ if __name__ == "__main__":
 
     # --------------------------- Randomise --------------------------
 
-    best_yet = 9999999
-    N = int(input("How many times do you want to run the randomise algorithm?\n"))
+    # best_yet = 9999999
+    # N = int(input("How many times do you want to run the randomise algorithm?\n"))
 
-    for _ in range(N):
-        # create a new game
-        game = Game(6, "data/Rushhour6x6_1.csv")
+    # for _ in range(N):
+    #     # create a new game
+    #     game = Game(6, "data/Rushhour6x6_1.csv")
         
-        # move cars around randomly until the game is won
-        while not game.won():
-            car = random_car(game.cars)
-            game.move(car.name, random_move(car, game.board))
+    #     # move cars around randomly until the game is won
+    #     while not game.won():
+    #         car = random_car(game.cars)
+    #         game.move(car.name, random_move(car, game.board))
 
-        # if this solution has the least amount of moves yet, overwrite the output
-        if len(game.moves) < best_yet:
-            best_yet = len(game.moves)
-            game.output()
-            game.draw_board()
+    #     # if this solution has the least amount of moves yet, overwrite the output
+    #     if len(game.moves) < best_yet:
+    #         best_yet = len(game.moves)
+    #         game.output()
+    #         game.draw_board()
 
-    game.draw_board()
+    # game.draw_board()
 
     # --------------------------- Manual -----------------------------
 
-    # while not game.won():
-    #     print('Instructions: Left and down are negative numbers, right and up are positive numbers')
-    #     car = input("Please the name of the car that should be moved:\n")
-    #     if car == "stop":
-    #         break
+    while not game.won():
+        print('Instructions: Left and down are negative numbers, right and up are positive numbers')
+        car = input("Please the name of the car that should be moved:\n")
+        if car == "stop":
+            break
             
-    #     direction = input("Please enter your move from -4 to 4:\n")
-    #     game.move(car.upper(), int(direction))
-    #     game.draw_board()
-    #     game.output()
+        direction = input("Please enter your move from -4 to 4:\n")
+        game.move(car.upper(), int(direction))
+        game.draw_board()
+    
+    game.output()
