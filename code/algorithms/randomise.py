e07873a655b0a4cc2e0aebf00a987788dab4d451
sanderbroos <sander.broos@student.uva.nl>
2021-01-12 15:48:29 +0100

    made output shorter by combining moves

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -2,18 +2,11 @@ import random
 from code.util import *
 
 def random_car(cars):
-    car = random.choice(list(cars.values()))
-    
-    return car
+    return random.choice(list(cars.values()))
 
 def random_move(car, board):
     possibilities = get_possiblities(car, board)
     if not possibilities:
         return None
 
-    move = random.choice(possibilities)
-    return move
-
-
-
-
+    return random.choice(possibilities)
