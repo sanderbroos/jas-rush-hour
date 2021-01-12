8f18d7c8a516948b8e5b444c13c8e3acf514bc9c
sanderbroos <sander.broos@student.uva.nl>
2021-01-12 16:12:55 +0100

    Loop randomise algorithm to find best result

--- main.py
+++ main.py
@@ -8,10 +8,25 @@ if __name__ == "__main__":
     print('Instructions: Left and down are negative numbers, right and up are positive numbers')
     game.draw_board()
 
-    while not game.won():
-        car = random_car(game.cars)
-        move = random_move(car, game.board)
-        game.move(car.name, move)
+    best_yet = 9999999
+    N = int(input("How many times do you want to run the randomise algorithm?\n"))
+
+    for i in range(N):
+        # create a new game
+        game = Game(6, "data/Rushhour6x6_1.csv")
+        
+        # move cars around randomly until the game is won
+        while not game.won():
+            car = random_car(game.cars)
+            move = random_move(car, game.board)
+            game.move(car.name, move)
+
+        # if this solution has the least amount of moves yet, overwrite the output
+        if len(game.moves) < best_yet:
+            best_yet = len(game.moves)
+            game.output()
+            game.draw_board()
+            
     game.draw_board()
 
 
@@ -24,4 +39,4 @@ if __name__ == "__main__":
     #     game.move(car.upper(), int(direction))
     #     game.draw_board()
 
-    game.output()
+    
