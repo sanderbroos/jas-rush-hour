0f26287b45e1285355b7384914140e9260087256
Jamie Faber <jamiefaber@hotmail.com>
2021-01-26 02:33:37 +0100

    cleanup

--- code/classes/car.py
+++ code/classes/car.py
@@ -11,11 +11,10 @@ class Car:
         self.col = col
         self.row = row
         self.length = length
-        self.colour = str
 
-        # gets a list of colours to choose from and removes red and black
-        colourslist = list(set(colors.cnames.values()) - {'#FF0000', '#000000'})
         if self.name == 'X':
                 self.colour = 'r'
+        # gets a list of colours to choose from and removes red and black
         else:
+            colourslist = list(set(colors.cnames.values()) - {'#FF0000', '#000000'})
             self.colour = random.choice(colourslist)
